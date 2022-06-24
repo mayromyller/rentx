9202b2ef8e3d5ff3db9c73a7bcd91cd50e6b1eaf
Mayro <deehmyller11@gmail.com>
2022-06-24 11:22:50 -0300

    chore: add fonts load, theme and routes for App

--- App.tsx
+++ App.tsx
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+
+import React, { useEffect, useState } from 'react'
+import * as SplashScreen from 'expo-splash-screen'
+import { StatusBar } from 'expo-status-bar'
+import * as Font from 'expo-font'
+
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold
+} from '@expo-google-fonts/inter'
+import {
+  Archivo_400Regular,
+  Archivo_500Medium
+} from '@expo-google-fonts/archivo'
+
+import { ThemeProvider } from 'styled-components'
+
+import { Routes } from './src/routes/index'
+
+import theme from './src/styles/theme'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [appIsReady, setAppIsReady] = useState(false)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync()
+
+        await Font.loadAsync({
+          Inter_400Regular,
+          Inter_500Medium,
+          Inter_600SemiBold,
+          Archivo_400Regular,
+          Archivo_500Medium
+        })
+      } catch (error) {
+        console.log(
+          `The app doesn't load fonts async, with this error: ${error}`
+        )
+      } finally {
+        setAppIsReady(true)
+        await SplashScreen.hideAsync()
+      }
+    }
+
+    prepare()
+  }, [])
+
+  if (!appIsReady) {
+    return null
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar style="dark" translucent backgroundColor="transparent" />
+      <Routes />
+    </ThemeProvider>
+  )
+}
