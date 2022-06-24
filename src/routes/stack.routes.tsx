255d2fe50ce3efef174050fbe3572c6bdc6b324b
Mayro <deehmyller11@gmail.com>
2022-06-24 11:12:20 -0300

    chore: add stack route navigation

---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,25 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import { Scheduling } from '../screens/Scheduling'
+import { Summary } from '../screens/Summary'
+import { Done } from '../screens/Done'
+import { Home } from '../screens/Home'
+import { Car } from '../screens/Car'
+
+const Stack = createNativeStackNavigator()
+
+export function StackRoutes() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Stack.Screen name="home" component={Home} />
+      <Stack.Screen name="car" component={Car} />
+      <Stack.Screen name="scheduling" component={Scheduling} />
+      <Stack.Screen name="summary" component={Summary} />
+      <Stack.Screen name="done" component={Done} />
+    </Stack.Navigator>
+  )
+}
