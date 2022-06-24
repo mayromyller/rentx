548e0861572730b2622affed27c1cb3110d64b2f
Mayro <deehmyller11@gmail.com>
2022-06-24 11:13:18 -0300

    chore: add global tyoe for react navigation

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      home: undefined
+      car: undefined
+      scheduling: undefined
+      summary: undefined
+      done: undefined
+    }
+  }
+}
