ce6590c950ee1641adfad8f862d204efdc0d229a
Mayro <deehmyller11@gmail.com>
2022-06-27 10:17:27 -0300

    chore: pass car data for routes

--- src/screens/Car/index.tsx
+++ src/screens/Car/index.tsx
@@ -1,23 +1,24 @@
 import React from 'react'
 
-import { useNavigation } from '@react-navigation/native'
+import { StatusBar } from 'react-native'
+
+import { useNavigation, useRoute } from '@react-navigation/native'
 
 import { BackButton } from '../../components/BackButton'
-import { Item } from '../../components/Item'
+import { Button } from '../../components/Button'
 import { Slider } from '../../components/Slider'
+import { Item } from '../../components/Item'
 
-import SpeedSVG from '../../assets/speed.svg'
-import AccelerationSVG from '../../assets/acceleration.svg'
-import ForceSVG from '../../assets/force.svg'
-import GasolineSVG from '../../assets/gasoline.svg'
-import ExchangeSVG from '../../assets/exchange.svg'
-import PeoplelSVG from '../../assets/people.svg'
+import { CarNavigationProps } from '../../@types/navigation'
 
 import * as S from './style'
-import { Button } from '../../components/Button'
+import IconType from '../../utils/carIcons'
 
 export function Car() {
   const navigation = useNavigation()
+  const route = useRoute()
+
+  const data = route.params as CarNavigationProps
 
   function handleBack() {
     navigation.goBack()
@@ -27,8 +28,24 @@ export function Car() {
     navigation.navigate('scheduling')
   }
 
+  const {
+    about,
+    brand,
+    name,
+    rent,
+    thumbnail,
+    fuel_type,
+    accessories,
+    photos
+  } = data.car
+
   return (
     <S.Container>
+      <StatusBar
+        barStyle="dark-content"
+        translucent
+        backgroundColor="transparent"
+      />
       <S.Header>
         <BackButton onPress={handleBack} />
       </S.Header>
@@ -44,30 +61,23 @@ export function Car() {
       <S.Content>
         <S.Flex>
           <S.Box>
-            <S.Brand>Lamborghini</S.Brand>
-            <S.Name>Huracan</S.Name>
+            <S.Brand>{brand}</S.Brand>
+            <S.Name>{name}</S.Name>
           </S.Box>
 
           <S.Box>
-            <S.Text>Ao dia</S.Text>
-            <S.Heading>R$ 580</S.Heading>
+            <S.Text>{rent.period}</S.Text>
+            <S.Heading>{rent.price}</S.Heading>
           </S.Box>
         </S.Flex>
 
         <S.Grid>
-          <Item name="380km/h" icon={SpeedSVG} />
-          <Item name="3.2s" icon={AccelerationSVG} />
-          <Item name="800 HP" icon={ForceSVG} />
-          <Item name="Gasolina" icon={GasolineSVG} />
-          <Item name="Auto" icon={ExchangeSVG} />
-          <Item name="2 pessoas" icon={PeoplelSVG} />
+          {accessories.map((item) => (
+            <Item key={item.name} name={item.name} icon={IconType(item.type)} />
+          ))}
         </S.Grid>
 
-        <S.Paragraph>
-          Este é automóvel desportivo. Surgiu do lendário touro de lide
-          indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
-          para quem gosta de acelerar.
-        </S.Paragraph>
+        <S.Paragraph>{about}</S.Paragraph>
       </S.Content>
 
       <S.Footer>
