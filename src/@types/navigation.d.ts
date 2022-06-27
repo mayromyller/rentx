ba1c37c89c3427d26ec8da3ecd6e2dffec7ad7a0
Mayro <deehmyller11@gmail.com>
2022-06-27 10:22:03 -0300

    chore: add type CarProps in the navigation to receive data car

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,8 +1,14 @@
+import { CarProps } from '../components/Car'
+
+export type CarNavigationProps = {
+  car: CarProps
+}
+
 export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       home: undefined
-      car: undefined
+      car: CarNavigationProps
       scheduling: undefined
       summary: undefined
       done: undefined
