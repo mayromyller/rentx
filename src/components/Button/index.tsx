34132af29edb8656e3a8d9f7427f2360a76299f0
Mayro <deehmyller11@gmail.com>
2022-07-01 08:01:02 -0300

    refact: add new properties for loading and disabled in Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,38 @@
 import React from 'react'
 
-import { TouchableOpacityProps } from 'react-native'
+import { TouchableOpacityProps, ActivityIndicator } from 'react-native'
 
 import * as S from './style'
 
 type Props = TouchableOpacityProps & {
   title: string
   color?: string
+  loading?: boolean
+  disabled?: boolean
 }
 
-export function Button({ title, color, ...rest }: Props) {
+export function Button({
+  title,
+  color,
+  loading,
+  disabled = false,
+  ...rest
+}: Props) {
   return (
-    <S.Container color={color} {...rest} activeOpacity={0.8}>
-      <S.Title>{title}</S.Title>
+    <S.Container
+      color={color}
+      {...rest}
+      activeOpacity={0.8}
+      disabled={disabled}
+      style={{
+        opacity: disabled ? 0.5 : 1
+      }}
+    >
+      {loading ? (
+        <ActivityIndicator color="white" />
+      ) : (
+        <S.Title>{title}</S.Title>
+      )}
     </S.Container>
   )
 }
