7d05333132c8cb0c71479d0dd4bf6cddcf55c800
Mayro <deehmyller11@gmail.com>
2022-07-01 08:03:02 -0300

    chore: add mycars in navigation type and other types for Car and Summary screen

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,16 +1,22 @@
 import { CarProps } from '../components/Car'
 
-export type CarNavigationProps = {
+export type DetailsCarNavigation = {
   car: CarProps
+}
+
+export type SummaryNavigationProps = {
+  car: CarProps
+  dates: string[]
 }
 
 export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       home: undefined
-      car: CarNavigationProps
-      scheduling: undefined
-      summary: undefined
+      mycars: undefined
+      car: DetailsCarNavigation
+      scheduling: DetailsCarNavigation
+      summary: SummaryNavigationProps
       done: undefined
     }
   }
